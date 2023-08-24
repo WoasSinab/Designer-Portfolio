a0657188f2da4798426cf1e0934a9a73e174c986
Sina Beyki <mrsaher83@gmail.com>
2023-08-24 22:38:23 +0330

    responsive fixed.

--- src/Components/ProjectOne/ProjectOne.jsx
+++ src/Components/ProjectOne/ProjectOne.jsx
@@ -1,9 +1,9 @@
 const ProjectOne = () => {
   return (
     // Project one 1
-    <div className='flex md:flex-row flex-col-reverse bg-one mx-2 my-8 rounded-2xl text-black/90'>
+    <div className='flex lg:flex-row flex-col-reverse bg-one mx-2 my-8 rounded-2xl text-black/90'>
       {/* Contents  */}
-      <div className='md:w-1/2 flex flex-col justify-between w-full'>
+      <div className='lg:w-1/2 flex flex-col justify-between w-full'>
         <div className='m-6'>
           <p className='text-black/60 mt-4'>UI | UX Design</p>
           <h1 className='font-semibold text-5xl my-6'>Digishop</h1>
@@ -14,13 +14,13 @@ const ProjectOne = () => {
         </div>
 
         <div className='m-6'>
-          <p className='leading-loose md:pr-32'>At our design agency, we pride ourselves on crafting exceptional User Interface (UI) and User Experience (UX) designs for e-commerce websites. Our team understands the critical role that design plays in creating a seamless and delightful shopping experience for users.</p>
+          <p className='leading-loose lg:pr-32'>At our design agency, we pride ourselves on crafting exceptional User Interface (UI) and User Experience (UX) designs for e-commerce websites. Our team understands the critical role that design plays in creating a seamless and delightful shopping experience for users.</p>
           <button className='p-4 border border-black py-3 rounded-xl my-8 hover:bg-purple-600 hover:border-purple-500 transition duration-300 hover:text-white'>See Project</button>
         </div>
       </div>
 
       {/* img  */}
-      <div className='md:w-1/2 flex md:justify-end w-full'>
+      <div className='lg:w-1/2 flex lg:justify-end w-full'>
         <img src='https://s6.uupload.ir/files/image2_dwn.png' alt='' className='p-8' />
       </div>
     </div>
--- src/Components/ProjectTwo/ProjectTwo.jsx
+++ src/Components/ProjectTwo/ProjectTwo.jsx
@@ -1,9 +1,9 @@
 const ProjectTwo = () => {
   return (
     // Project one 1
-    <div className='flex md:flex-row flex-col-reverse bg-Two mx-2 my-8 rounded-2xl text-white/90'>
+    <div className='flex lg:flex-row flex-col-reverse bg-Two mx-2 my-8 rounded-2xl text-white/90'>
       {/* Contents  */}
-      <div className='md:w-1/2 flex flex-col justify-between w-full'>
+      <div className='lg:w-1/2 flex flex-col justify-between w-full'>
         <div className='m-6'>
           <p className='text-white/60 mt-4'>UI | UX Design</p>
           <h1 className='font-semibold text-5xl my-6'>Golestan ReDesign</h1>
@@ -14,13 +14,13 @@ const ProjectTwo = () => {
         </div>
 
         <div className='m-6'>
-          <p className='leading-loose md:pr-32'>At our design studio, we are excited to share our recent endeavor in revamping the User Interface (UI) and User Experience (UX) design of the Sistan and Baluchestan University website. Our team embarked on this project with a dedication to enhancing the online journey of students, faculty, and visitors</p>
+          <p className='leading-loose lg:pr-32'>At our design studio, we are excited to share our recent endeavor in revamping the User Interface (UI) and User Experience (UX) design of the Sistan and Baluchestan University website. Our team embarked on this project with a dedication to enhancing the online journey of students, faculty, and visitors</p>
           <button className='p-4 border border-white py-3 rounded-xl my-8 hover:bg-purple-600 hover:border-purple-500 transition duration-300 hover:text-white'>See Project</button>
         </div>
       </div>
 
       {/* img  */}
-      <div className='md:w-1/2 flex md:justify-end w-full'>
+      <div className='lg:w-1/2 flex lg:justify-end w-full'>
         <img src='https://s6.uupload.ir/files/image3_tlh.png' alt='' className='p-8' />
       </div>
     </div>
